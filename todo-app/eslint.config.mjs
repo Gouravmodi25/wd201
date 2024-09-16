d691f20e0a39acd8b0d9036ec8705d9bcd0b8a03
Gouravmodi25 <gouravmodi39@gmail.com>
2024-09-17 01:29:45 +0530

    add todo-app

--- todo-app/eslint.config.mjs
+++ todo-app/eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  {
+    ignores: ["./migrations", "./models"],
+  },
   pluginJs.configs.recommended,
 ];
